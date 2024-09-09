e31fb2af0cce1ccf333318d72856e11a7c6b3149
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2024-09-09 17:57:12 -0300

    ending countdown and make initial challenge box

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,11 +1,16 @@
 'use client'
 
 import { useEffect, useState } from 'react'
+import { ShieldCheck } from 'lucide-react'
 import styles from '../styles/components/Countdown.module.css'
 
+let coutndownTimeout: NodeJS.Timeout;
+
 export function Countdown(){
-    const [time, setTime] = useState(25 * 60);
-    const [active, setActive] = useState(false);
+    const timeOfExecution = 0.05 * 60 //Para 25 minutos de exercício
+    const [time, setTime] = useState(timeOfExecution);
+    const [isActive, setIsActive] = useState(false);
+    const [hasFinished, setHasFinished] = useState(false);
 
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
@@ -14,16 +19,24 @@ export function Countdown(){
     const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
 
     function startCountdown(){
-        setActive(true)
+        setIsActive(true)
+    }
+    function resetCountdown(){
+        clearTimeout(coutndownTimeout)
+        setIsActive(false)
+        setTime(timeOfExecution)
     }
 
     useEffect(() => {
-        if(active && time > 0){
-            setTimeout(() => {
+        if(isActive && time > 0){
+            coutndownTimeout = setTimeout(() => {
                 setTime(time - 1);
             }, 1000)
+        } else if (isActive && time === 0){
+            setHasFinished(true);
+            setIsActive(false);
         }
-    }, [active, time])
+    }, [isActive, time])
 
     return(
         <div>
@@ -38,10 +51,30 @@ export function Countdown(){
                     <span>{secondRight}</span>
                 </div>
             </div>
-            
-            <button type='button' className={styles.countdownButton} onClick={startCountdown}>
-                Iniciar um ciclo
-            </button>
+
+            { hasFinished ? (
+                <button disabled className={styles.countdownButton}>
+                    <div className='flex flex-col flex-1 items-center h-[70%] mt-7 rounded-[5px ] justify-between'>
+                        <div className='flex items-center gap-4'>
+                            Ciclo Encerrado
+                            <ShieldCheck fill='#4CD62B' />
+                        </div>
+                        <div className='h-1 bg-[#4CD62B] w-full'></div>
+                    </div>
+                </button>
+            ) : (
+                <>
+                    { isActive ? (
+                        <button type='button' className={`${styles.countdownButton} ${styles.countdownButtonActive}`} onClick={resetCountdown}>
+                            Abandonar Ciclo
+                        </button>
+                    ) : (
+                        <button type='button' className={styles.countdownButton} onClick={startCountdown}>
+                            Iniciar um ciclo
+                        </button>
+                    )}   
+                </>
+            )}         
         </div>
     )
 }
